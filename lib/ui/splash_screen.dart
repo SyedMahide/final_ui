f736aa6dcbbd3745842efb0e32581e4448a082cc
DESKTOP-0HQK47B\Mahide <mahidekarim@gmail.com>
2022-09-04 12:51:23 +0600

    splash screen

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -30,8 +30,11 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Column(
           children: [
-            const  Image(
-              image: AssetImage("assets/splash.png"),
+            Container(
+              margin: EdgeInsets.only(top: 180),
+              child: const  Image(
+                image: AssetImage("assets/splash.png"),
+              ),
             ),
             Container(
               alignment: Alignment.center,
@@ -47,12 +50,12 @@ class _SplashScreenState extends State<SplashScreen> {
             ),
             Container(
               alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 14),
+              margin: EdgeInsets.only(top: 8),
                 child: const Text("A sister concern of WECON properties",
                   style: TextStyle(
                     fontFamily: 'Quantico',
                     fontWeight: FontWeight.w700,
-                    fontSize: 20,
+                    fontSize: 14,
                     color: Colors.white,
                   ),
                 )
